28aca7a7e1e95c761ae532a44011c59e25c6b07d
maulana-tech <firdaussyah03@gmail.com>
2026-05-27 19:57:51 +0700

    feat(foundry/src/FhenixYieldVault.sol): auto update

--- foundry/src/FhenixYieldVault.sol
+++ foundry/src/FhenixYieldVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.24;
 
 import { FHE, euint64, InEuint64 } from "@fhenixprotocol/cofhe-contracts/FHE.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
