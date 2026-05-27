7958712d4a9728c7bc4983930e16df782dddfe47
maulana-tech <firdaussyah03@gmail.com>
2026-05-27 19:57:51 +0700

    feat(foundry/test/FhenixYieldVault.t.sol): auto update

--- foundry/test/FhenixYieldVault.t.sol
+++ foundry/test/FhenixYieldVault.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.24;
 
 import { CofheTest } from "@cofhe/foundry-plugin/contracts/CofheTest.sol";
 import { CofheClient } from "@cofhe/foundry-plugin/contracts/CofheClient.sol";
